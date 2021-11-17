507e381261fd374218bafbfb57d61750505afa4f
Pradnya <pradnyathakur2002@gmail.com>
2021-11-17 11:41:05 +0530

    second commit

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&hgvjwzk96d-8-#3l&dn3^canifhkv+(re2(d680^i-)b@0ht1'
+SECRET_KEY = '***'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
